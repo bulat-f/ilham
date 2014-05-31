ab99ab9876860c4658cc179c09ab5ac2abd8415f
Fatbulat <fatbulat5@gmail.com>
2014-05-31 13:08:16 +0400

    Первичные статические страницы
    - Созданы статические страницы 'index', 'about', 'help'
    - Созданы шаблоны (layouts) _foter.html.haml, _header.html.haml
    - Настроен bootstrap (включая javascript)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(title)
+    base = "Ilham"
+    if title.blank?
+      title = base
+    else
+      title = base + " | " + title
+    end
+    return title
+  end
 end
