d686adf3f5048b4730d022aea60ab77e3f227354
Fatbulat <fatbulat5@gmail.com>
2014-08-06 11:44:41 +0400

    Тестовый коммит на новой сервер

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,35 +9,7 @@ set :rvm_ruby_version, '2.1.2'
 set :deploy_to, '/var/www/apps/ilham'
 set :rails_env, 'production'
 
-namespace :foreman do
-  desc 'Start server'
-  task :start do
-    on roles(:all) do
-      sudo "start #{application}"
-    end
-  end
-
-  desc 'Stop server'
-  task :stop do
-    on roles(:all) do
-      sudo "stop #{application}"
-    end
-  end
-
-  desc 'Restart server'
-  task :restart do
-    on roles(:all) do
-      sudo "restart #{application}"
-    end
-  end
-
-  desc 'Server status'
-  task :status do
-    on roles(:all) do
-      execute "initctl list | grep #{application}"
-    end
-  end
-end
+slide_count = 3
 
 namespace :git do
   desc 'Deploy'
@@ -80,29 +52,36 @@ end
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+namespace :unicorn do
 
+  desc 'Start'
+  task :start do
+    on roles(:all) do
+      execute "cd #{current_path} && bundle exec unicorn_rails -c #{current_path}/config/unicorn.rb -E production -D"
+    end
+  end
+end
 namespace :deploy do
 
  desc 'Setup'
   task :setup do
     on roles(:all) do
       execute "mkdir  #{shared_path}/config/"
+      execute "mkdir  #{shared_path}/img/"
       execute "mkdir  /var/www/apps/#{application}/run/"
       execute "mkdir  /var/www/apps/#{application}/log/"
-      execute "mkdir  /var/www/apps/#{application}/socket/"
       execute "mkdir #{shared_path}/system"
-      sudo "ln -s /var/log/upstart /var/www/log/upstart"
 
       upload!('shared/database.yml', "#{shared_path}/config/database.yml")
 
-      upload!('shared/Procfile', "#{shared_path}/Procfile")
-
+      slide_count.times do |i|
+        upload!("shared/slide#{i}.jpg",  "#{shared_path}/img/slide#{i}.jpg")
+      end
 
       upload!('shared/nginx.conf', "#{shared_path}/nginx.conf")
-      sudo 'stop nginx'
-      sudo "rm -f /usr/local/nginx/conf/nginx.conf"
-      sudo "ln -s #{shared_path}/nginx.conf /usr/local/nginx/conf/nginx.conf"
-      sudo 'start nginx'
+      sudo 'service nginx stop'
+      sudo "ln -sf #{shared_path}/nginx.conf /etc/nginx/conf.d/default.conf"
+      sudo 'service nginx start'
 
       within release_path do
         with rails_env: fetch(:rails_env) do
@@ -116,25 +95,10 @@ namespace :deploy do
   task :symlink do
     on roles(:all) do
       execute "ln -sf #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-      execute "ln -sf #{shared_path}/Procfile #{release_path}/Procfile"
       execute "ln -sf #{shared_path}/system #{release_path}/public/system"
-    end
-  end
-
-  desc 'Foreman init'
-  task :foreman_init do
-    on roles(:all) do
-      foreman_temp = "/var/www/tmp/foreman"
-      execute  "mkdir -p #{foreman_temp}"
-      # Создаем папку current для того, чтобы foreman создавал upstart файлы с правильными путями
-      execute "ln -s #{release_path} #{current_path}"
-
-      within current_path do
-        execute "cd #{current_path}"
-        execute :bundle, "exec foreman export upstart #{foreman_temp} -a #{application} -u deployer -l /var/www/apps/#{application}/log -d #{current_path}"
+      slide_count.times do |i|
+        execute "ln -sf #{shared_path}/img/slide#{i}.jpg #{release_path}/public/slide#{i}.jpg"
       end
-      sudo "mv #{foreman_temp}/* /etc/init/"
-      sudo "rm -r #{foreman_temp}"
     end
   end
 
@@ -142,7 +106,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      sudo "restart #{application}"
+      sudo "service nginx restart"
     end
   end
 
@@ -150,12 +114,6 @@ namespace :deploy do
   after :finishing, 'deploy:restart'
 
   after :updating, 'deploy:symlink'
-
-  after :setup, 'deploy:foreman_init'
-
-  after :foreman_init, 'foreman:start'
-
-  before :foreman_init, 'rvm:hook'
 
   before :setup, 'deploy:starting'
   before :setup, 'deploy:updating'
