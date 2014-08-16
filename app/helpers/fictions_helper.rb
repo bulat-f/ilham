e459e22ccde6b5b8cb6a83107cdaa5f55c05ba9e
Fatbulat <fatbulat5@gmail.com>
2014-08-16 23:07:48 +0400

    Улучшены представления
    - Кнопка "Читать" для купленных произведений
    - Класс .prose для описания и эпизодов

--- app/helpers/fictions_helper.rb
+++ app/helpers/fictions_helper.rb
@@ -1,2 +1,11 @@
 module FictionsHelper
+
+  def first_episode_path(fiction)
+    first = fiction.episodes.sort.first
+    if first
+      fiction_episode_path(fiction, first)
+    else
+      '#'
+    end
+  end
 end
