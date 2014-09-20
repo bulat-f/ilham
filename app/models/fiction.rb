5f69c7fcd8e5145d9241f992fa2d035513a68a28
Fatbulat <fatbulat5@gmail.com>
2014-09-20 21:45:26 +0400

    Сделал разбиение на строки публикация и произведений

--- app/models/fiction.rb
+++ app/models/fiction.rb
@@ -8,17 +8,6 @@ class Fiction < ActiveRecord::Base
 
   mount_uploader :cover, CoverUploader
 
-  def self.list(col = 3)
-    count = self.count
-    result = Array.new(count / col + (count % col != 0 ? 1 : 0)) { Array.new }
-    tmp = self.all
-    tmp.each_index do |i|
-      result[i / col].push(tmp[i])
-    end
-
-    return result
-  end
-
   def t_genre
     I18n.t("genres.#{self.genre.name}")
   end
